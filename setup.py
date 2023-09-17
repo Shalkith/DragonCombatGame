7147ea40c4b7786de18899befa95e2d3b746324d
Paul Giles <pgwar2@gmail.com>
2023-09-16 19:38:38 -0700

    update setup

--- setup.py
+++ setup.py
@@ -6,54 +6,55 @@ import os
 import json
 import random
 import datetime
-
+import damage_chart
+import dragonlatter
 # Import the necessary modules from the same directory as this file
-from config import JSON_DIR, DRAGON_JSON, CHALLENGES_JSON, COMBAT_LOG_JSON, DAMAGE_CHART_JSON
+import config
 
 # Create a function that will create the json_files directory if it doesn't exist
 def create_json_dir():
     # check if the json_files directory exists
-    if os.path.isdir(JSON_DIR):
+    if os.path.isdir(config.JSON_DIR):
         # if it does exist, do nothing
         pass
     else:
         # if it doesn't exist, create it
-        os.mkdir(JSON_DIR)
+        os.mkdir(config.JSON_DIR)
 
 # Create a function that will create the dragon.json file if it doesn't exist
 def create_dragon_json():
     # check if the dragon.json file exists
-    if os.path.isfile(JSON_DIR+"/"+DRAGON_JSON):
+    if os.path.isfile(config.dragonjson):
         # if it does exist, do nothing
         pass
     else:
         # if it doesn't exist, create it
         data = {"dragons": []}
-        with open(JSON_DIR+"/"+DRAGON_JSON, "w") as file:
+        with open(config.dragonjson, "w") as file:
             json.dump(data, file, indent=4)
 
 # Create a function that will create the challenges.json file if it doesn't exist
 def create_challenges_json():
     # check if the challenges.json file exists
-    if os.path.isfile(JSON_DIR+"/"+CHALLENGES_JSON):
+    if os.path.isfile(config.challengesjson):
         # if it does exist, do nothing
         pass
     else:
         # if it doesn't exist, create it
         data = {"challenges": []}
-        with open(JSON_DIR+"/"+CHALLENGES_JSON, "w") as file:
+        with open(config.challengesjson, "w") as file:
             json.dump(data, file, indent=4)
 
 # Create a function that will create the combat_log.json file if it doesn't exist
 def create_combat_log_json():
     # check if the combat_log.json file exists
-    if os.path.isfile(JSON_DIR+"/"+COMBAT_LOG_JSON):
+    if os.path.isfile(config.combatlogjson):
         # if it does exist, do nothing
         pass
     else:
         # if it doesn't exist, create it
         data = {"combat_log": []}
-        with open(JSON_DIR+"/"+COMBAT_LOG_JSON, "w") as file:
+        with open(config.combatlogjson, "w") as file:
             json.dump(data, file, indent=4)
 
 
@@ -62,14 +63,9 @@ def generate_dragons():
         import hatching_dragons as hd
 
 
-        for i in range(45):
-            #select a random breed
-            breed = hd.random_breed()
-            #select a random name
-            name = hd.random_name()
-
+        for i in range(1):
             ownerid = 'cpu'        
-            hd.generatedragons(name,breed,ownerid,1)
+            hd.generatedragons('name','breed',ownerid,1)
 
 #setup the environment
 if __name__ == "__main__":
@@ -78,3 +74,5 @@ if __name__ == "__main__":
     create_challenges_json()
     create_combat_log_json()
     #generate_dragons()
+    damage_chart.generate_damagechart()
+    dragonlatter.create_dragon_html()
